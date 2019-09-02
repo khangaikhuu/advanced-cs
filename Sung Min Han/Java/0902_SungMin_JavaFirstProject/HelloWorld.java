67006873df6d3e6bd7cee557cefe80ee8cb01641
G12 <g12@asu.local>
2019-09-02 09:46:37 +0800

    0902_SungMin_JavaFirstProject

---
+++ Sung Min Han/Java/0902_SungMin_JavaFirstProject/HelloWorld.java
@@ -0,0 +1,48 @@
+
+/**
+ * Hello world! This is my first lesson of advanced Java
+ *
+ * @author (Sung Min)
+ * @version (1.0)
+ */
+public class HelloWorld
+{
+    //my first variable
+    private int myInt = 10;
+    //my second variable
+    private long myLong = 1232213131;
+    private String myString = "Hello World";
+    private boolean myBoolean = true;
+    private char myChar = 'C';
+    private double myDouble = 10.0;
+    private float myFloat = 10.0f;
+    
+    public int getMyInt()
+    {
+        return myInt;
+    }
+    public long getMyLong()
+    {
+        return myLong;
+    }
+    public String getMyString()
+    {
+        return myString;
+    }
+    public boolean getMyBoolean()
+    {
+        return myBoolean;
+    }
+    public char getMyChar()
+    {
+        return myChar;
+    }
+    public double getMyDouble()
+    {
+        return myDouble;
+    }
+    public float getMyFloat()
+    {
+        return myFloat;
+    }
+}
