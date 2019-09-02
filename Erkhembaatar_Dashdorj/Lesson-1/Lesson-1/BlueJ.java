b7610bd9c913c4ffcc7e9b3ad606157d58409466
Erkhembaatar Dashdorj <erkhembaatar.d@asu.edu.mn>
2019-09-02 14:00:30 +0800

    wqe:

---
+++ Erkhembaatar_Dashdorj/Lesson-1/Lesson-1/BlueJ.java
@@ -0,0 +1,79 @@
+
+/**
+ * Java Class
+ *
+ * @author (Erkhembaatar_Dashdorj)
+ * @version (1.0)
+ */
+public class BlueJ
+{
+    private int myInt = 10;
+    
+    private long myLong= Integer.MAX_VALUE;
+    
+    private String myString = "Hello World";
+    
+    private boolean myBoolean = false;
+    
+    private char myChar = 'C';
+    
+    private double myDouble = 4.5;
+    
+    private float myFloat = 10.5f;
+    public static void main(String[] args)
+    {
+       
+        System.out.println("Heloo Worlsd");
+    }
+    public int getInt() 
+    {
+        return myInt;
+    }
+    public String getString()
+    {
+        return myString;
+    }
+    public boolean getBoolean()
+    {
+        return myBoolean;
+    }
+    public char getChar()
+    {
+        return myChar;
+    }
+    public float getFloat()
+    {
+        return myFloat;
+    }
+    public long getLong()
+    {
+        return myLong;
+    }
+    public double getDouble()
+    {
+        return myDouble;
+    }
+    
+    
+    
+    
+   /* // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BlueJ
+     */
+ 
+    /*
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+    
+    public int sampleMethod(int y)
+    {
+       // put your code here
+        return x + y;
+    }
+    */
+}
