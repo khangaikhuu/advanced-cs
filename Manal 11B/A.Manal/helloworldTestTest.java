40315e55158bfbc21555dacad4576d61693b48d6
JutsuMeister <skullcicator@gmail.com>
2019-09-02 09:28:01 +0800

    as

---
+++ Manal 11B/A.Manal/helloworldTestTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class helloworldTestTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class helloworldTestTest
+{
+    /**
+     * Default constructor for test class helloworldTestTest
+     */
+    public helloworldTestTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
