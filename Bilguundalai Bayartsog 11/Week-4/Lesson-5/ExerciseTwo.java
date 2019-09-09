7ee2903075fe6b7dc44121ebc4fb18ac382f89ec
G12 <g12@asu.local>
2019-09-09 09:36:08 +0800

    d

---
+++ Bilguundalai Bayartsog 11/Week-4/Lesson-5/ExerciseTwo.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class ExerciseTwo here.
+ *
+ * @author (Bilguundalai)
+ * @version (0.9.0.9.2019)
+ */
+public class ExerciseTwo
+{
+    private int a = 10;
+    private double c = 23.0;
+    
+    public int getSum()
+    {
+        if (a > 20)
+        {
+            a += 10;
+        }
+        else
+        {
+            a += 20;
+        }
+        return a;
+    }
+}
