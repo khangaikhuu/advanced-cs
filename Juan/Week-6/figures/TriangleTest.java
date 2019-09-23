d2e8413459824f6e29e611c1f210019e332d8980
G12 <g12@asu.local>
2019-09-23 09:58:59 +0800

    #12 Inheritence Ex-1

---
+++ Juan/Week-6/figures/TriangleTest.java
@@ -0,0 +1,22 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TriangleTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TriangleTest
+{
+    @Test
+    public void test()
+    {
+    Triangle ex = new Triangle();
+    ex.makeVisible();
+    }
+}
