c5e2357753044cae4771d5bcd7f1652e95b49d83
AnandOkrrr <anandbayarbat@gmail.com>
2019-09-24 09:32:10 +0800

    #15,#16

---
+++ mr.khangai/Week-2309-2709/figures/CircleTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class CircleTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CircleTest
+{
+    /**
+     * Default constructor for test class CircleTest
+     */
+    public CircleTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
