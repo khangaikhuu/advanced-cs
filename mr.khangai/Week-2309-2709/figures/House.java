17d8b6916b8b35340c589a9744a8eb740860629b
G8 <G8@asu.local>
2019-09-24 09:38:46 +0800

    #21 House Picture

--- mr.khangai/Week-2309-2709/figures/House.java
+++ mr.khangai/Week-2309-2709/figures/House.java
@@ -7,5 +7,32 @@
  */
 public class House
 {
-   
+   public void House() 
+   { 
+      Square c5 = new Square(); 
+      c5.makeVisible(); 
+      c5.changeSize(150); 
+      c5.moveHorizontal(-250);
+      Square c4 = new Square(); 
+      c4.makeVisible(); 
+      c4.changeColor("black"); 
+      c4.changeSize(45);
+      c4.moveHorizontal(-225);
+      c4.moveVertical(30);
+      Triangle c3 = new Triangle(); 
+      c3.makeVisible(); 
+      c3.changeColor("green");
+      c3.changeSize(90,275);
+      c3.moveVertical(-85);
+      c3.moveHorizontal(-70);
+      Circle c2 = new Circle(); 
+      c2.makeVisible(); 
+      c2.changeColor("yellow");
+      c2.changeSize(100);
+      c2.moveHorizontal(70);
+      c2.moveVertical(-30);
+      
+      
+      
+    }
 }
