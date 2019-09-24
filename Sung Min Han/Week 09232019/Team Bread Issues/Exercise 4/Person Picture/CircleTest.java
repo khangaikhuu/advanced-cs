86e555fd8afbf8f8a47c78f1cf4a3476abbee201
G12 <g12@asu.local>
2019-09-24 09:43:22 +0800

    #22_SungMinJasonTsengel

--- Sung Min Han/Week 09232019/Team Bread Issues/Exercise 4/Person Picture/CircleTest.java
+++ Sung Min Han/Week 09232019/Team Bread Issues/Exercise 4/Person Picture/CircleTest.java
@@ -22,5 +22,21 @@ public class CircleTest
        c1.moveHorizontal(125);
        c1.changeSize(50);
        c1.moveVertical(25);
+       
+       Circle c3 = new Circle(); 
+       c3.makeVisible(); 
+       c3.changeColor("green");
+       c3.changeSize(1000);
+       c3.moveVertical(125);
+       c3.moveHorizontal(-500);
+       
+       Person c2 = new Person(); 
+       c2.makeVisible(); 
+       c2.changeSize(50,25);
+       c2.moveRight();
+       
+       Person c4 = new Person(); 
+       c4.makeVisible(); 
+       c4.changeSize(50,25);
     }
 }
