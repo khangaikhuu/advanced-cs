0111d1c4823cb521662accf23eb823dec770d4ee
G12 <g12@asu.local>
2019-10-03 09:38:26 +0800

    student java\

--- TeamKEA/Khulan/src/main/java/mn/asu/crud/repository/StudentRepository.java
+++ TeamKEA/Khulan/src/main/java/mn/asu/crud/repository/StudentRepository.java
@@ -1,4 +1,12 @@
 package mn.asu.crud.repository;
+import java.util.List;
 
-public class StudentRepository {
+import mn.asu.crud.entity.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface StudentRepository extends CrudRepository <Student, Long> {
+    List<Student> findByName(String name);
 }
