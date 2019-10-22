306f0ad00752ec6ca87544640634c59e30831b88
G12 <g12@asu.local>
2019-10-22 08:36:29 +0800

    khulan changes

--- TeamKEA/Project/src/main/java/mn/asu/teamKEA/Main.java
+++ TeamKEA/Project/src/main/java/mn/asu/teamKEA/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args)
     {
         JFrame frame = new JFrame();
-        JLabel title = new JLabel("Let's learn French");
+        JLabel title = new JLabel("Let's Learn French");
         title.setBounds(450, -100, 1000, 700);
         frame.add(title);
-        JButton startButton = new JButton("start");
+        JButton startButton = new JButton("Start");
         startButton.setBounds(430,550,150,70);
         frame.add(startButton);
         frame.setSize(1600, 900);
