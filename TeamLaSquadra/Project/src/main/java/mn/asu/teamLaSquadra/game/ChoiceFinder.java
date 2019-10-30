630ada94bc5041d546b6e76fe7eb6534cd55dbfe
Juan <zolmonjuancarlos@gmail.com>
2019-10-30 09:52:02 +0800

    new button system working

--- TeamLaSquadra/Project/src/main/java/mn/asu/teamLaSquadra/game/ChoiceFinder.java
+++ TeamLaSquadra/Project/src/main/java/mn/asu/teamLaSquadra/game/ChoiceFinder.java
@@ -1,39 +1,42 @@
 package mn.asu.teamLaSquadra.game;
 
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class ChoiceFinder {
-    public void choiceSelect(boolean boolChoice[]) throws FileNotFoundException {
+    private Choice choice;
+    public void choiceSelect(boolean choice1, boolean choice2, boolean choice3, boolean choice4, boolean choice5, boolean choice6, boolean choice7, boolean choice8, boolean choice9, Button exitButton) throws FileNotFoundException{
 
 
-        if (buttonCounter1 == 1 && buttonCounter2 == 1) {
+
+        if (choice4) {
             Image sceneKampf;
             try {
-                nextChoice(sceneKampf = new Image(new FileInputStream("Project/src/main/resources/prison.png")),
+                choice.nextChoice(sceneKampf = new Image(new FileInputStream("Project/src/main/resources/prison.png")),
                         "The next day in prison is here.", "get out of bed", "exit");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-            System.out.println(buttonCounter1);
         }
 
-        if (buttonCounter1 == 2  && buttonCounter2 == 1) {
+
+        if (choice6) {
             Image sceneDayPrison;
             try {
-                nextChoice(sceneDayPrison = new Image(new FileInputStream("Project/src/main/resources/dayprison.jpg")),
+                choice.nextChoice(sceneDayPrison = new Image(new FileInputStream("Project/src/main/resources/dayprison.jpg")),
                         "The guard is calling you out.", "yes", "exit");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
 
-        if (buttonCounter1 == 3  && buttonCounter2 == 1) {
+        if (choice8) {
             Image sceneDayPrison;
             try {
-                nextChoice(sceneDayPrison = new Image(new FileInputStream("Project/src/main/resources/leavingPrison.jpg")),
+                choice.nextChoice(sceneDayPrison = new Image(new FileInputStream("Project/src/main/resources/leavingPrison.jpg")),
                         "The Bavarian Supreme Cour has pardoned your treason, " +
                                 "/n and you are released from prison after only a year, in 20 December 1924.", "Pack up your stuff and leave", "Leave");
             } catch (FileNotFoundException e) {
@@ -42,5 +45,4 @@ public class ChoiceFinder {
         }
     }
 
-}
 }
