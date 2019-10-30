4a7f876df96c54310bd78f5f8395b19de6ac4290
G12 <g12@asu.local>
2019-10-30 09:22:55 +0800

    create repositories

--- Team2/Project/src/main/java/mn/asu/crud/team2/controller/ValueCreationController.java
+++ Team2/Project/src/main/java/mn/asu/crud/team2/controller/ValueCreationController.java
@@ -1,6 +1,6 @@
 package mn.asu.crud.team2.controller;
 
-import mn.asu.crud.team2.repository.MarketingRepository;
+
 import mn.asu.crud.team2.repository.ValueCreationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,15 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/value")
 public class ValueCreationController {
+    private final ValueCreationRepository valueCreationRepository;
 
     @Autowired
     public ValueCreationController(ValueCreationRepository valueCreationRepository) {
-        this.ValueCreationRepository = valueCreationRepository;
+        this.valueCreationRepository = valueCreationRepository;
     }
 
     @GetMapping("/index")
     public String showValueCreationPage(Model model) {
-        model.addAttribute("valueCreations", ValueCreationRepository.findAll());
+        model.addAttribute("valueCreations", valueCreationRepository.findAll());
         return "ValueCreation";
     }
 }
--- Team2/Project/src/main/java/mn/asu/crud/team2/repository/ValueCreationRepository.java
+++ Team2/Project/src/main/java/mn/asu/crud/team2/repository/ValueCreationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ValueCreationRepository xtends CrudRepository<ValueCreationEntity, Long> {
+public interface ValueCreationRepository extends CrudRepository<ValueCreationEntity, Long> {
     List<ValueCreationEntity> findByName(String name);
 }
