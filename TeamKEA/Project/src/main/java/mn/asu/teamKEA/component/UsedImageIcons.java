85e9acde9e255c71a4d460c6101030053701c4cd
G12 <g12@asu.local>
2019-10-31 09:20:01 +0800

    resources

--- TeamKEA/Project/src/main/java/mn/asu/teamKEA/component/UsedImageIcons.java
+++ TeamKEA/Project/src/main/java/mn/asu/teamKEA/component/UsedImageIcons.java
@@ -1,16 +1,26 @@
 package mn.asu.teamKEA.component;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 
-public class UsedImageIcons
+public class UsedImageIcons {
 
-{
-    public UsedImageIcons()
+    public static Image ourImage;
     {
+
+        File sourceImage = new File("resources/static/Pariss.jpg");
+        ourImage = ImageIO.read(sourceImage);
+    }
+
+
+
+    public UsedImageIcons() throws IOException {
 
     }
 
-    public static ImageIcon chanter = new ImageIcon("C:\\Users\\G12\\Downloads\\TEAMKEA_RESOURCES\\slide_2.jpg");
+    public static ImageIcon chanter = new ImageIcon(ourImage);
 
     public static ImageIcon penser = new ImageIcon("C:\\Users\\G12\\Pictures\\slide_3.jpg");//C:\Users\G12\Downloads\TEAMKEA RESOURCES
 }
