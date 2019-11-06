4c6efe96d32e900de51c99a2d0822b0b672af188
G12 <g12@asu.local>
2019-11-06 09:44:07 +0800

    #106 Business add

--- Team2/Project/src/main/java/mn/asu/crud/team2/controller/businessController.java
+++ Team2/Project/src/main/java/mn/asu/crud/team2/controller/businessController.java
@@ -5,8 +5,12 @@ import mn.asu.crud.team2.repository.BusinessRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/business")
@@ -34,5 +38,15 @@ public class businessController {
     @GetMapping("/signup")
     public String showSignUpForm(BusinessEntity businessEntity) {
         return "Add-Business";
+    }
+
+    @PostMapping("/add")
+    public String addStudent(@Valid BusinessEntity businessEntity, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "Add-Business";
+        }
+
+        businessRepository.save(businessEntity);
+        return "redirect:crud";
     }
 }
