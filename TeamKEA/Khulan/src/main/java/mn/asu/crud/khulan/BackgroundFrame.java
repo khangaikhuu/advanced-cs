d336bd0f113111e75e9a4d1dde0039a6b0f0bd64
MyangaaN <reapercool@yahoo.com>
2019-11-13 09:54:25 +0800

    #131, 116

--- TeamKEA/Khulan/src/main/java/mn/asu/crud/khulan/BackgroundFrame.java
+++ TeamKEA/Khulan/src/main/java/mn/asu/crud/khulan/BackgroundFrame.java
@@ -1,11 +1,11 @@
 package mn.asu.crud.khulan;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class BackgroundFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("TitleLessJFrame");
-        JButton al = new JButton("Exit");
         frame.getContentPane().add(new JLabel("Let's Learn French!"));
         frame.setUndecorated(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
