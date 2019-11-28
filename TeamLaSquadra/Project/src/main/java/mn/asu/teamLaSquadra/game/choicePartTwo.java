10389f13481939d7775a092d9b46d284562085cb
khangarid_altangadas <garid3@gmail.com>
2019-11-28 09:18:18 +0800

    #139 #140

--- TeamLaSquadra/Project/src/main/java/mn/asu/teamLaSquadra/game/choicePartTwo.java
+++ TeamLaSquadra/Project/src/main/java/mn/asu/teamLaSquadra/game/choicePartTwo.java
@@ -1,7 +1,9 @@
 package mn.asu.teamLaSquadra.game;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class choicePartTwo {
@@ -11,12 +13,28 @@ public class choicePartTwo {
 
         if(input==15)
         {
-
+            Image backPrison;
+            try {
+                choice.nextChoice(backPrison = new Image(new FileInputStream(getClass().getClassLoader().getResource("prison.png").getFile())),
+                        "You are back in prison for disagreeing with the Prime Minister." +
+                                "\n Retard", "Okay", "Exit");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            pressed=15;
         }
 
         if(input==16)
         {
-
+            Image freedom;
+            try {
+                choice.nextChoice(freedom = new Image(new FileInputStream(getClass().getClassLoader().getResource("freedom.png").getFile())),
+                        "Nice! You are a completely free man now as long as you" +
+                                "\n abide by the state's laws", "Okay", "Exit");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            pressed=16;
         }
         if(input==17)
         {
