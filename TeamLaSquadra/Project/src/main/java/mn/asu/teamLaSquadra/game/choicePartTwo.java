53d5f0485d1815ff142c78e1a773ab783d291646
G8 <G8@asu.local>
2019-11-28 09:26:11 +0800

    #143 General Info

--- TeamLaSquadra/Project/src/main/java/mn/asu/teamLaSquadra/game/choicePartTwo.java
+++ TeamLaSquadra/Project/src/main/java/mn/asu/teamLaSquadra/game/choicePartTwo.java
@@ -2,14 +2,16 @@ package mn.asu.teamLaSquadra.game;
 
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class choicePartTwo {
     private int pressed = 0;
+    private Prologue information = new Prologue();
 
-    public int choiceSelect(int input, Button exitButton, Choice choice) throws FileNotFoundException {
+    public int choiceSelect(int input, Button exitButton, Choice choice, Stage primaryStage) throws FileNotFoundException {
 
         if(input==15)
         {
@@ -38,6 +40,16 @@ public class choicePartTwo {
         }
         if(input==17)
         {
+            try {
+                information.changer(primaryStage, "You are now playing the first chapter of a historical WW2 game" +
+                        "\n you will play as Adolf Hitler in the first chapter and your goal is to reach the highest seat of power" +
+                        "\n in order to play this game it is both educational and at the same time assessing your knowledge on the topics" +
+                        "\n presented. You must know which choices will lead to your claim to power or your downfall" +
+                        "\n you must CHOOSE carefully or else you will lose.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
 
         }
         return pressed;
