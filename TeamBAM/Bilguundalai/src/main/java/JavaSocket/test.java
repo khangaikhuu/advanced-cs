ec6c93efb0e8dbae2b8c7fbe2d649ee1a978bac4
Erkhembaatar Dashdorj <erkhembaatar.d@asu.edu.mn>
2019-12-16 10:01:15 +0800

    Extra Credit

---
+++ TeamBAM/Bilguundalai/src/main/java/JavaSocket/test.java
@@ -0,0 +1,18 @@
+package JavaSocket;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class test {
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
+    }
+
+    private void assertEquals(String hello_client, String response) {
+    }
+}
