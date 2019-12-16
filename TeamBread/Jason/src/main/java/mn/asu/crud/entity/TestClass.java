fb0761e22cd3c267cd9f729a25695a447f854989
Jason_Lee <jhl0625@gmail.com>
2019-12-16 09:24:54 +0800

    extraCredit Jason

---
+++ TeamBread/Jason/src/main/java/mn/asu/crud/entity/TestClass.java
@@ -0,0 +1,11 @@
+package mn.asu.crud.entity;
+import java.io.IOException;
+
+public class TestClass {
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
+    }
+}
