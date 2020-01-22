cf9ea491a01d5284957686390d5d36223b71c231
JutsuMeister <skullcicator@gmail.com>
2020-01-22 09:36:43 +0800

    a

---
+++ TeamBAM/Project/src/main/java/mn/entity/WordsForm.java
@@ -0,0 +1,15 @@
+package mn.entity;
+
+import java.util.ArrayList;
+
+public class WordsForm {
+    private ArrayList<String> clientList;
+
+    public ArrayList<String> getClientList() {
+        return clientList;
+    }
+
+    public void setClientList(ArrayList<String> clientList) {
+        this.clientList = clientList;
+    }
+}
